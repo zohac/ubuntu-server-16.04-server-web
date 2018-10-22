7d183bfb91967785270d964eb50dd21f11ba8091
zohac <fenrir0680@gmail.com>
2018-10-22 18:23:17 +0200

    feat(core): add xdebug_installation.sh file

---
+++ xdebug_installation.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Version 0.1
+
+#
+# Installation de xdebug
+#
+printf "%s" "${GREEN}"
+echo ''
+echo '###########################################'
+echo '#           xdebug installation           #'
+echo '###########################################'
+echo ''
+echo ''
+printf "%s" "${NORMAL}"
+
+sudo pecl install xdebug
+
+# setup Xdebug
+sudo -s <<eof
+ echo '
+xdebug.show_error_trace = 1
+' >> /etc/php/7.1/mods-available/xdebug.ini
+eof
+
+sudo -s <<eof
+echo '
+;;;;;;;;;;;;;;;;;;;;
+;      xdebug      ;
+;;;;;;;;;;;;;;;;;;;;
+zend_extension="/usr/lib/php/20160303/xdebug.so"
+
+xdebug.remote_enable = On
+' >> /etc/php/7.1/apache2/php.ini
+eof
+
+sudo -s <<eof
+echo '
+;;;;;;;;;;;;;;;;;;;;
+;      xdebug      ;
+;;;;;;;;;;;;;;;;;;;;
+zend_extension="/usr/lib/php/20160303/xdebug.so"
+
+xdebug.remote_enable = On
+' >> /etc/php/7.1/cli/php.ini
+eof
+
+sudo service apache2 restart
