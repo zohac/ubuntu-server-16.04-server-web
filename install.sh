3ec608452376b5d1297b95c5c97a02c194ca5b14
zohac <fenrir0680@gmail.com>
2018-12-30 18:13:42 +0100

    add a dialog box

--- install.sh
+++ install.sh
@@ -31,37 +31,62 @@ echo -e "$GREEN"' | | /| / / _ \/ / ___/ __ \/ __ `__ \/ _ \ '
 echo -e "$GREEN"' | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/ '
 echo -e "$GREEN"' |__/|__/\___/_/\___/\____/_/ /_/ /_/\___/  '
 echo ''
-echo -e "$GREEN"'Installing Apache2/PHP7.3/MySQL for a development web server on ubuntu server 18.04.'
+echo -e "$GREEN"'Installing Apache2/PHP7.3/MySQL for a development web server on ubuntu server.'
 echo ''
 echo ''
 echo -e "$NORMAL"
 
-DEPENDENCIES=(
-    "update.sh"
-    "dependencies_installation.sh"
-    "apache2_installation.sh"
-    "mysql_installation.sh"
-    "php7.3_installation.sh"
-    "xdebug_installation.sh"
-    "composer_installation.sh"
-    "php-cs-fixer_installation.sh"
-    "php-code-sniffer_installation.sh"
-    "php-mess-detector_installation.sh"
-    "copy-paste-detector_installation.sh"
-    "samba_installation.sh"
-    "blackFire_installation.sh"
-    "zsh_installation.sh"
-    "cleaning.sh"
-)
+echo -e "$BLUE"'Update...'"$NORMAL"
+#sudo apt-get -y update
+echo -e "$BLUE"'Installation of Dialog...'"$NORMAL"
+#sudo apt-get install -y dialog
 
-# get length of $distro array
-length=${#DEPENDENCIES[@]}
+DIALOG="${DIALOG=dialog}"
+TEMP_FILE=`tempfile 2>/dev/null` || TEMP_FILE=/tmp/test$$
+trap "rm -f $TEMP_FILE" 0 1 2 5 15
 
-# Use bash for loop
-for (( i=0; i<$length; i++ ));
-    do
-        source "$FULL_DIR"/dependencies/"${DEPENDENCIES[$i]}" ;
-    done
+"$DIALOG" --backtitle "Choisissez les composants à installer" \
+        --title "Choisissez les composants à installer" --clear \
+        --checklist "Selectionner les composants avec la barre espace" 25 80 15 \
+        "update" "Mise à jour" ON \
+        "dependencies" "Installation des dépendances" ON \
+        "apache2" "Installation du serveur http" ON \
+        "mysql" "Installation de la base de donnée" ON \
+        "php7.3" "Installation de php7.3" ON \
+        "xdebug" "Outil de debugage et profilage pour php" ON \
+        "composer" "Manager de dépendance pour php" ON \
+        "php-cs-fixer" "PHP Coding Standards Fixer" ON \
+        "php-code-sniffer" "PHP Code sniffer" ON \
+        "php-mess-detector" "PHP Mess Detector" ON \
+        "copy-paste-detector" "Détecteur de code copier/coller" ON \
+        "samba" "Partage de fichiers dans un réseau local" ON \
+        "blackFire" "Solution de management de performance pour php" ON \
+        "zsh" "Installation du shell zsh" ON \
+        "cleaning" "Nettoyage de l'installation" ON 2> "$TEMP_FILE"
+
+DIALOG_RESPONSE="$?"
+CHOICE=`cat "$TEMP_FILE"`
+
+case "$DIALOG_RESPONSE" in
+    0)
+        # Convert CHOICE into ARRAY
+        IFS=' ' read -r -a ARRAY_CHOICE <<< "$CHOICE"
+        # get length of ARRAY_CHOICE
+        length=${#ARRAY_CHOICE[@]}
+
+        # Use bash for loop
+        for (( i=0; i<$length; i++ ));
+            do
+                source "$FULL_DIR"/dependencies/"${DEPENDENCIES[$i]}"_installation.sh ;
+            done
+        ;;
+    1)
+        echo "Appuyé sur Annuler.";;
+    255)
+        echo "Appuyé sur Echap.";;
+esac
+
+clear
 
 echo -e "$GREEN"' _____         __                    '
 echo -e "$GREEN"'/__  /  ____  / /_  ____ ______      '
@@ -91,6 +116,7 @@ do
         ;;
         *)
             echo -e "$RED"'Error, you had to answer yes[Y] or no[n].'"$NORMAL"
+            RESPONSE=''
         ;;
     esac
 
